c4453f7a19c814d3b2f672850f947e932be6edd4
dwianto27 <56668975+dwianto27@users.noreply.github.com>
2022-09-21 05:05:20 +0700

    Create tamp.js

---
+++ tamp.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Newscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Username
+// @match        http://mylinkhere.link/download/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.seconds = -1;
+})();// ==UserScript==
+// @name         Newscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Username
+// @match        http://mylinkhere.link/download/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.seconds = -1;
+})();
